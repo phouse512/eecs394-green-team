899bbc6b7f1e21d6a57b035d850746c4951d5541
Kevin Ye <kevin.ye204@gmail.com>
2013-10-05 02:24:25 +0100

    Added black, blue, red, green, to css

--- js/source.js
+++ js/source.js
@@ -15,17 +15,34 @@ function createPost(postText, postColor) {
 
 
 function viewPosts(){
-	/*$.ajax({
-		url: 'script/viewAllPosts.php',
+	//test with example.xml off-server
+	/*
+	$.ajax({
+		url: 'example.xml',
 		type: 'GET',
 		success: function(data, textStatus, xhr) {
-			console.log(data);
+			$(data).find('post').each(function(index){
+				var text = $(this).find('post_text').text();
+				var color_class = $(this).find('post_color').text();
+				if(color_class.localeCompare("0") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote blackNote">' + text + '</div>');
+				} else if(color_class.localeCompare("1") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote blueNote">' + text + '</div>');
+				} else if(color_class.localeCompare("2") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote redNote">' + text + '</div>');
+				} else if(color_class.localeCompare("3") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote greenNote">' + text + '</div>');
+				}
+			});
+			//var total = $('*', data).length;
+			//console.log(total);
 		}, 
 		error: function(xhr, textStatus, errorThrown){
 			alert(textStatus);
 		} 
-	});
-		*/
+	});*/
+		
+		
 	$.ajax({
 		url: 'script/viewAllPosts.php',
 		type: 'GET',
@@ -33,7 +50,15 @@ function viewPosts(){
 		success: function(data, textStatus, xhr) {
 			$(data).find('post').each(function(){
 				var text = $(this).find('post_text').text();
-				$('#noteSpace').append('<div class="col-xs-3 postNote">' + text + '</div>');
+				if(color_class.localeCompare("0") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote blackNote">' + text + '</div>');
+				} else if(color_class.localeCompare("1") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote blueNote">' + text + '</div>');
+				} else if(color_class.localeCompare("2") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote redNote">' + text + '</div>');
+				} else if(color_class.localeCompare("3") == 0){
+					$('#noteSpace').append('<div class="col-xs-3 postNote greenNote">' + text + '</div>');
+				}
 			});
 			//var total = $('*', data).length;
 			//console.log(total);
